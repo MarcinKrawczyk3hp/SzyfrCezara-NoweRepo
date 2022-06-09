01107a61d4243755a9476cb778e95e9099bd2b60
3109754 <3109754@TE-GDA-D-D37H11>
2022-06-09 13:19:42 +0200

    23

--- Szyfr/Program.cs
+++ Szyfr/Program.cs
@@ -1,9 +1,11 @@
-﻿Console.WriteLine("Wektor przesunięcia = 3");
-Console.WriteLine("Szyfr Cezara");
-
-string wpisz, wypisz;
+﻿string wpisz, wypisz;
 char[] litery = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
+Console.WriteLine("Wektor przesunięcia = ");
+int c = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Szyfr Cezara");
+Console.WriteLine("Podaj co ma być zaszyfrowane");
 
 wpisz = Convert.ToString(Console.ReadLine());
 char[] z = wpisz.ToCharArray();
@@ -16,8 +18,17 @@ for (int i = 0; i < z.Length; i++)
 
     if (z[i] == ' ' || z[i] == ' ') continue;
     int a = Array.IndexOf(litery, litera);
-    int nowaPozycja = (a + 3) % 26;
-    char szyfr = litery[nowaPozycja];
+    int nowaPozycja = (a + c);
+    char szyfr;
+    if(nowaPozycja >= 23)
+    {
+        szyfr = litery[nowaPozycja - 26];
+    }
+    else
+    {
+        szyfr = litery[nowaPozycja];
+    }
+    
     x[i] = szyfr;
 }
 
